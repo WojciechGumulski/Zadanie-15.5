73a8a2364fe35f74e4e0b8a042909c399ccec732
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-03-30 23:51:08 +0200

    Add files

---
+++ components/App.js
@@ -0,0 +1,73 @@
+App = React.createClass({
+    getInitialState() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+    handleSearch: function(searchingText) {  
+        this.setState({
+            loading: true  
+        });
+        this.getGif(searchingText, function(gif) {  
+            this.setState({  
+                loading: false,  
+                gif: gif,  
+                searchingText: searchingText  
+            });
+        }.bind(this));
+    },
+
+    getGif: function(searchingText, callback) {
+        return new Promise ( 
+            (resolve, reject) => {
+                const GIPHY_PUB_KEY = 'vL0XZUL2TlbhD3wH2W27UOWvAkT4xIX5';
+                const GIPHY_API_URL = 'https://api.giphy.com';
+                let url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
+                let xhr = new XMLHttpRequest();  
+                xhr.open('GET', url);
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        let data = JSON.parse(xhr.responseText).data; 
+                        let gif = {  
+                            url: data.fixed_width_downsampled_url,
+                            sourceUrl: data.url
+                        };
+                        resolve(gif);  
+                    }
+                    reject(new Error(this.statusText));
+                };
+                xhr.onerror = function () {
+                    reject(new Error(`XMLHttpRequest Error: ${this.statusText}`));
+                };
+                xhr.send();
+            }
+        );
+    },
+    
+    render: function() {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return (
+            <div style={styles}>
+                <h1>Wyszukiwarka GIFów!</h1>
+                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+                <Search 
+                    onSearch={this.handleSearch}
+                />
+                <Gif 
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
+                </div>
+        );
+    }
+});
